f95be4a194d8f3dc9f29791c5c5784e82c94a401
kevin <kevin.pastran.d@gmail.com>
2025-08-10 19:14:41 -0400

    Correcion de la generacion de lista de gastos. El cambio mas contundente es el LazyColumn

--- app/src/main/java/com/example/finanzas/ui/features/ListOperation/OperationRoute.kt
+++ app/src/main/java/com/example/finanzas/ui/features/ListOperation/OperationRoute.kt
@@ -1,12 +1,18 @@
 package com.example.finanzas.ui.features.ListOperation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.finanzas.R
 import com.example.finanzas.model.categoria.Categoria
@@ -21,6 +27,7 @@ fun OperactionRoute(
     val state by viewModel.uiState.collectAsState()
 
     Column {
+        Spacer(modifier = Modifier.padding(16.dp))
         CategoryRail(
             selected = selectedCategory,
             onCategorySelected = { categoria ->
@@ -34,16 +41,23 @@ fun OperactionRoute(
         )
 
         // Mostrar lista de gastos con icono de su categoría
-        state.gastos.forEach { gasto ->
-            val categoriaEnum = gasto.categoria?.let { nombre ->
-                Categoria.values().find { it.name == nombre }
-            }
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
+        ) {
+            items(state.gastos) { gasto ->
+                val categoriaEnum = gasto.categoria?.let { nombre ->
+                    Categoria.values().find { it.name == nombre }
+                }
 
-            OperacionScreen(
-                gasto = gasto,
-                icon = categoriaEnum?.icono ?: R.drawable.shopping
-            )
+                OperacionScreen(
+                    gasto = gasto,
+                    icon = categoriaEnum?.icono ?: R.drawable.shopping
+                )
+            }
         }
+
     }
 }
 
